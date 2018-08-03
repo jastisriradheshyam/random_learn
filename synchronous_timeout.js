44344b639096438a589c8380979b98a6af10d0dc
Jasti Sri Radhe Shyam <jastisriradheshyam@users.noreply.github.com>
2018-08-03 15:10:47 +0530

    Create synchronous_timeout.js

---
+++ synchronous_timeout.js
@@ -0,0 +1,27 @@
+/*
+Usage:
+-------------------------------------------------
+var syn_timeout = require('synchronous_timeout');
+//some code
+await syn_timeout.pause(300);
+//some code
+-------------------------------------------------
+  if defined and used in a same file:
+-------------------------------------------------
+//some code
+await pause(300);
+//some code
+-------------------------------------------------
+*/
+
+var pause = async function(milliseconds) {
+	var dt = new Date();
+	while ((new Date()) - dt <= milliseconds) { /* Do nothing */ }
+}
+
+exports.pause = pause;
+
+/*
+Credits: Nathan
+Site: https://stackoverflow.com/questions/4122268/using-settimeout-synchronously-in-javascript
+*/
