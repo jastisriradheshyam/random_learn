a0d245da615c8e1b2625d74560a4c8050638d597
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2018-11-22 01:22:34 +0530

    fix match regex

--- regex.js
+++ regex.js
@@ -5,6 +5,6 @@ var StringToMatch = "23-someString"
 n = StringToMatch.match(/^(\d+)-someString$/)[1]
 // n will be 23 due to (\d+) 
 
-n = StringToMatch.match(/^(\d+)-someString$/)[1]
+n = StringToMatch.match(/^(\d+)-someString$/)
 // n will be [ '23-someString', '23', index: 0, input: '23-someString' ]
 // ------ Match | End | ------
